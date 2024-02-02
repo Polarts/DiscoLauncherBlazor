04b06561fe80abad2f95cf8e1dd963901caadd4f
Polarts <davidklein.4496@gmail.com>
2024-02-02 23:03:39 +0200

    Added MainWindow2 to try transparent web view

---
+++ Launcher/MainWindow2.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components.WebView;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow2.xaml
+    /// </summary>
+    public partial class MainWindow2 : Window
+    {
+        public MainWindow2()
+        {
+            InitializeComponent();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+
+            MainWebView.BlazorWebViewInitialized += WebViewInitialzed;
+        }
+
+        private void WebViewInitialzed(object? sender, BlazorWebViewInitializedEventArgs e)
+        {
+            e.WebView.DefaultBackgroundColor = System.Drawing.Color.Transparent;
+        }
+    }
+}
