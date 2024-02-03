4d14f303bf691e97d7b3902aa2902f4306f2508c
Polarts <davidklein.4496@gmail.com>
2024-02-03 23:02:09 +0200

    Added svg defs and animations, "pseudo-transparency"

--- Launcher/MainWindow2.xaml.cs
+++ Launcher/MainWindow2.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components.WebView;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,7 @@ namespace Launcher
             InitializeComponent();
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddBlazorWebViewDeveloperTools();
             Resources.Add("services", serviceCollection.BuildServiceProvider());
 
             MainWebView.BlazorWebViewInitialized += WebViewInitialzed;
