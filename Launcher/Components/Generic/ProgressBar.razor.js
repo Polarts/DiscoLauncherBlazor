da8bd7d89c69580de171a3839fc1936a90667833
Polarts <polarbear.oc@gmail.com>
2024-03-06 17:30:09 +0200

    Added progress bar impl.

---
+++ Launcher/Components/Generic/ProgressBar.razor.js
@@ -0,0 +1,8 @@
+﻿const RECT_COUNT = 10;
+const RECT_GAP = 6;
+
+export function ProgressBar(rootElement, dotNetObjRef) {
+    const clientRect = rootElement.getBoundingClientRect();
+    const rectWidth = (clientRect.width / RECT_COUNT) - RECT_GAP;
+    rootElement.style.setProperty('--rect-width', rectWidth+'px');
+}
